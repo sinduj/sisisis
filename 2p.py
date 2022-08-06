9e2e3699f9d4f95bfd15ec7c9ec2de8754a5430c
sinduj <110720240+sinduj@users.noreply.github.com>
2022-08-06 16:01:34 +0530

    create new commit MS-1

---
+++ 2p.py
@@ -0,0 +1,10 @@
+#sum and avg of 3 numbers
+a=3
+b=6
+c=7
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+c=int(input("enter value of c:"))
+sum=a+b+c
+avg=sum/3
+(print("sum,="sum,"avg=",avg))
